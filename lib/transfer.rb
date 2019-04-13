67754d1c34e8ca04fceb3431adcaea07f3b85468
Alex Merecka <merecka@gmail.com>
2019-04-13 00:57:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
       self.sender.balance -= amount
       self.receiver.balance += amount
       self.status = "complete"
-      binding.pry
+    #  binding.pry
     elsif self.sender.balance < amount
       "Transaction rejected. Please check your account balance."
       self.status = "rejected"
