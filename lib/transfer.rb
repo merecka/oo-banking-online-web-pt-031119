679a257cb428cef2d88db9a31f81042a71b6bb0a
Alex Merecka <merecka@gmail.com>
2019-04-13 01:03:34 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,8 +21,8 @@ class Transfer
 
   def execute_transaction
     if self.status != "complete" && self.sender.balance > self.amount
-      self.sender.balance -= amount
-      self.receiver.balance += amount
+      self.sender.balance -= self.amount
+      self.receiver.balance += self.amount
       self.status = "complete"
     #  binding.pry
     else
