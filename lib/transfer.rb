0d7c6055ef7aca619538544cbb550e1da65fc710
Alex Merecka <merecka@gmail.com>
2019-04-13 01:12:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Transfer
 
-  attr_accessor :sender, :receiver, :status, :amount
+  attr_accessor :sender, :receiver, :status, :amount, :last_amount, :last_sender, :last_receiver
 
   def initialize(sender, receiver, amount, status="pending")
     @sender = sender
@@ -21,6 +21,9 @@ class Transfer
 
   def execute_transaction
     if self.status != "complete" && self.sender.balance > self.amount
+      self.last_amount = self.amount
+      self.last_sender = self.sender
+      self.last_receiver = self.receiver
       self.sender.balance -= self.amount
       self.receiver.balance += self.amount
       self.status = "complete"
