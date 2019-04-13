19b59568e51260ddbd865b8b70c942fecc3d60de
Alex Merecka <merecka@gmail.com>
2019-04-13 01:18:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -31,7 +31,7 @@ class Transfer
   end
 
   def reverse_transfer
-    if self.status = "complete"
+    if self.status == "complete"
       self.sender.balance += self.amount
       self.receiver.balance -= self.amount
       self.status = "reversed"
